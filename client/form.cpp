335cf110f5572a295295907cfafc0218784a672f
Kropyva <anatolii.kropyvka@gmail.com>
2023-06-11 02:06:02 +0300

    Added an event handler for the login button, which establishes a connection to the server and allows us to enter if it is possible.

--- client/form.cpp
+++ client/form.cpp
@@ -1,11 +1,32 @@
 #include "form.h"
 #include "ui_form.h"
+#include "connect.h"
 
-Form::Form(QWidget *parent) :
+#include <QMessageBox>
+
+Form::Form(MainWindow& mainWin, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Form)
 {
     ui->setupUi(this);
+
+    QObject::connect(ui->buttonLogin, &QPushButton::clicked, this, [this, &mainWin](){
+        QJsonObject jsonObject {};
+        jsonObject["command"] = "login";
+        jsonObject["username"] = ui->lineUsername->text();
+        jsonObject["password"] = ui->linePassword->text();
+
+        QJsonDocument jsonDocument(jsonObject);
+        QByteArray jsonUtf8(jsonDocument.toJson(QJsonDocument::Compact));
+
+        if (!connect::login(jsonUtf8)) {
+            QMessageBox::warning(nullptr, "Warning", "Cannot login!");
+        } else {
+            mainWin.setJsonObject(jsonObject);
+            mainWin.show();
+            this->close();
+        }
+    });
 }
 
 Form::~Form()
